b583418e282374b1fccdeeafad3a016f0660c82d
Sahil Rathor <sr7015342@gmail.com>
2024-10-08 00:13:50 +0530

    added footer

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Auth from './pages/auth/Auth'
 import Home from './pages/Home'
@@ -7,13 +8,13 @@ import {  Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
-    <div className="w-full min-h-[100dvh] bg-gray-100 pb-20 center-items">
+    <div className="w-full min-h-[100dvh] bg-gray-100 ">
       <Navbar />
       <Routes>
         <Route path='/auth' element={<Auth />} />
         <Route path='/' element={<Home />} />
       </Routes>
-
+      <Footer />
     </div>
   )
 }
---
+++ FE/src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full h-20 mt-10 bg-primary text-white center-items'>
+        <p>Footer</p>
+    </div>
+  )
+}
+
+export default Footer
