46417a7ec6cee6dd77a1af3007ed831082e1121f
Sahil Rathor <sr7015342@gmail.com>
2024-10-13 20:44:01 +0530

    get productinfo api created

---
+++ BE/src/controllers/item/getItemInfo.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import Item from '../../models/itemModel';
+
+
+const getItemInfo = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        const item = await Item.findById(id);
+        res.status(200).json(item);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}   
+
+export default getItemInfo;
--- BE/src/routes/itemRoutes.ts
+++ BE/src/routes/itemRoutes.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import getItems from '../controllers/item/getItems'
+import getItemInfo from '../controllers/item/getItemInfo'
 import addItem from '../controllers/item/addItem'
 import deleteItem from '../controllers/item/deleteItem'
 import updateItem from '../controllers/item/updateItem'
@@ -7,6 +8,7 @@ import updateItem from '../controllers/item/updateItem'
 const router = express.Router()
 
 router.get('/', getItems);
+router.get('/:id', getItemInfo);
 router.post('/', addItem);
 router.delete('/:id', deleteItem);
 router.patch('/:id', updateItem);
