c473db30f3005739b17c3027485deee734957a08
Sahil Rathor <sr7015342@gmail.com>
2024-10-15 15:05:08 +0530

    links upadated

--- FE/src/components/footer/Footer.tsx
+++ FE/src/components/footer/Footer.tsx
@@ -5,6 +5,11 @@ import { FaLinkedinIn } from "react-icons/fa";
 import { MdArrowOutward } from "react-icons/md";
 
 const Footer = () => {
+  const linkedinLink = "https://www.linkedin.com/in/sahilrathor/"
+  const githubLink = "https://github.com/sahilrathor"
+  const instagramLink = "https://www.instagram.com/sahilrathor24/"
+  const sourceCodeLink = "https://github.com/sahilrathor/e-commerce"
+
   const { name } = useAppInfo()
   return (
     <div className='w-full border-t-[1px] px-12 py-10 border-slate-800/10 center-items justify-evenly gap-10'>
@@ -13,9 +18,13 @@ const Footer = () => {
         <span className='text-3xl font-bold text-emerald-500 mb-2 drop-shadow-lg'>{name}</span>
         <p className='text-sm font-semibold text-slate-800/50'>Shop smart, live better.</p>
         <p className='text-sm font-semibold text-slate-800/50'>Bringing the best to your doorstep</p>
-        <button className='w-44 text-lg text-white mt-5 bg-[#0d1117] border-[1px] border-slate-800/10 rounded-full py-2'>
-          Github
-        </button>
+        <a href={sourceCodeLink} target="_blank"
+          className='w-fit text-md font-semibold text-white mt-5 bg-[#272727] border-[1px] border-slate-800/10 rounded-full py-1 pl-3 pr-2 center-items gap-2 hover:bg-[#333333] transition-all duration-100'
+        >
+
+          Source Code
+          <FaGithub size={22}/>
+        </a>
       </div>
 
       {/* Links */}
@@ -40,40 +49,45 @@ const Footer = () => {
       <div className="socials">
         <div className="w-32 center-items items-start flex-col gap-2">
           {/* icon 1 */}
-          <div className="group flex gap-1 transition-all duration-300 cursor-pointer">
-            <a href="#"
-              className="bg-[#0077b5] rounded-lg p-1 text-white hover:bg-[#0077b5]/90 transition-all duration-100 "
-            >
+          <a href={linkedinLink} target="_blank"
+            className="group flex gap-1 transition-all duration-300 cursor-pointer"
+          >
+            <div
+              className="bg-[#0077b5] rounded-lg p-1 text-white hover:bg-[#0077b5]/90 transition-all duration-100 ">
               <FaLinkedinIn size={20} className="" />
-            </a>
+            </div>
             <span className="text-[#0077b5] font-semibold w-0 group-hover:w-full transition-all duration-300 overflow-hidden flex ">
               LinkedIn <MdArrowOutward size={14} />
             </span>
-          </div>
+          </a>
 
           {/* icon 2 */}
-          <div className="group flex gap-1 transition-all duration-300 cursor-pointer">
-            <a href="#"
-              className="bg-[#181717] rounded-lg p-1 text-white hover:bg-[#181717]/80 transition-all duration-100"
+          <a href={githubLink} target="_blank"
+            className="group flex gap-1 transition-all duration-300 cursor-pointer"
           >
+            <div
+              className="bg-[#181717] rounded-lg p-1 text-white hover:bg-[#181717]/80 transition-all duration-100 "
+            >
               <FaGithub size={20} />
-            </a>
+            </div>
             <span className="text-[#181717] font-semibold w-0 group-hover:w-full transition-all duration-300 overflow-hidden flex ">
               Github <MdArrowOutward size={14} />
             </span>
-          </div>
+          </a>
 
           {/* icon 3 */}
-          <div className="group flex gap-1 transition-all duration-300 cursor-pointer">
-            <a href="#"
-              className="bg-[#f33b78] rounded-lg p-1 text-white hover:bg-[#f33b78]/80 transition-all duration-100"
+          <a href={instagramLink} target="_blank"
+            className="group flex gap-1 transition-all duration-300 cursor-pointer"
           >
-            <FaInstagram size={20} />
-          </a>
+            <div
+              className="bg-[#f33b78] rounded-lg p-1 text-white hover:bg-[#f33b78]/80 transition-all duration-100  "
+            >
+              <FaInstagram size={20} />
+            </div>
             <span className="text-[#f33b78] font-semibold w-0 group-hover:w-full transition-all duration-300 overflow-hidden flex ">
               Instagram <MdArrowOutward size={14} />
             </span>
-          </div>
+          </a>
         </div>
       </div>
     </div>
