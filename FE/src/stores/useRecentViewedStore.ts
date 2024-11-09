c43cf520dd4dd8d9c2fdb8d08e6b8078fff0fd2e
Sahil Rathor <sr7015342@gmail.com>
2024-11-09 21:24:08 +0530

    recent viewed  store in local storage

--- FE/src/stores/useRecentViewedStore.ts
+++ FE/src/stores/useRecentViewedStore.ts
@@ -1,4 +1,21 @@
+// import { create } from 'zustand'
+// import { Item } from '../interfaces/item'
+
+// interface RecentViewedStore {
+//     recentlyViewedItems: Item[]
+//     setRecentlyViewedItems: (items: Item[]) => void
+// }
+
+// const useRecentViewedStore = create<RecentViewedStore>((set) => ({
+//     recentlyViewedItems: [],
+//     setRecentlyViewedItems: (items) => set({ recentlyViewedItems: items })
+// }))
+
+// export default useRecentViewedStore
+
+
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 import { Item } from '../interfaces/item'
 
 interface RecentViewedStore {
@@ -6,9 +23,16 @@ interface RecentViewedStore {
     setRecentlyViewedItems: (items: Item[]) => void
 }
 
-const useRecentViewedStore = create<RecentViewedStore>((set) => ({
-    recentlyViewedItems: [],
-    setRecentlyViewedItems: (items) => set({ recentlyViewedItems: items })
-}))
+const useRecentViewedStore = create<RecentViewedStore>()(
+    persist(
+        (set) => ({
+            recentlyViewedItems: [],
+            setRecentlyViewedItems: (items) => set({ recentlyViewedItems: items })
+        }),
+        {
+            name: 'recently-viewed-items',
+        }
+    )
+)
 
 export default useRecentViewedStore
